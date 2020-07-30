21e0cfdeb0ae806537087cfa00693b04735c491e
JTLiner925 <[jtliner925@gmail.com]>
2020-07-30 14:25:57 -0500

    added people to chat page

--- src/ChatPage/ChatPage.js
+++ src/ChatPage/ChatPage.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom";
 import './ChatPage.css'
 
 export default class ChatPage extends Component {
@@ -18,6 +19,24 @@ export default class ChatPage extends Component {
           <div className="chat-group">
             <div>Group #4</div>
           </div>
+        </div>
+        <div className="chat-person-banner">
+        <div className="chat-person">
+            <div>Person1</div>
+          </div>
+          <div className="chat-person">
+            <div>Person2</div>
+          </div>
+          <div className="chat-person">
+            <div>Person3</div>
+          </div>
+          <div className="chat-person">
+            <div>Person4</div>
+          </div>
+          <div className="chat-person">
+            <div>Person5</div>
+          </div>
+
         </div>
         <div className="chat-wall">
           <div className="chat-bubble">
