6cc4c7fd6a0fa0aca476e2e47c75cc0a47098884
JTLiner925 <[jtliner925@gmail.com]>
2020-08-02 16:11:24 -0500

    dashboard prayer card

--- src/DashHomePage/EventSelectComponent/EventSelectComponent.js
+++ src/DashHomePage/EventSelectComponent/EventSelectComponent.js
@@ -1,16 +1,28 @@
 import React from "react";
 import "./EventSelectComponent.css";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendarAlt,
+  faCalendarPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function EventSelectComponent() {
   return (
     <div className="Event-Select-Component">
-      <Link className="event-select-view" to="/events">
-        View Events
-      </Link>
-      <Link className="event-select-add" to="/add-event">
-        Add Event
-      </Link>
+      <div className="event-select-view">
+        <Link  to="/events">
+          <FontAwesomeIcon id="event-calendar-icon" icon={faCalendarAlt} />
+          <p>View Events</p>
+        </Link>
+        </div>
+        <div className="event-select-add">
+          <Link to="/add-event">
+            <FontAwesomeIcon id="add-event-icon" icon={faCalendarPlus} />
+          <p>Add Event</p>
+          </Link>
+        </div>
+      
     </div>
   );
 }
--- src/DashHomePage/PrayerWallComponent/PrayerWallComponent.js
+++ src/DashHomePage/PrayerWallComponent/PrayerWallComponent.js
@@ -1,28 +1,113 @@
-import React, { Component } from 'react'
-import './PrayerWallComponent.css'
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faUsers,
+  faHeart,
+  faImage,
+  faThumbsUp,
+  faCommentAlt,
+  faEllipsisH,
+  faImages,
+} from "@fortawesome/free-solid-svg-icons";
+import "./PrayerWallComponent.css";
 
 export default class PrayerWallComponent extends Component {
   render() {
     return (
       <div className="Prayer-Wall-Component">
-          <div className="prayer-wall-component-requests">
-            <div className="prayer-wall-component-request">
-              <div className="prayer-wall-component-prayer">Prayer Request #1</div>
+        <div className="prayer-wall-component-request">
+          <div className="prayer-header">
+            <div className="prayer-header-top">
+              <FontAwesomeIcon id="prayer-user-icon" icon={faUser} />
+              <div className="prayer-user">
+                <h2>JT Liner</h2>
+                <h3>
+                  Prayer <span className="prayer-days-ago">5 Days Ago</span>
+                </h3>
+              </div>
+              <FontAwesomeIcon id="prayer-users-icon" icon={faUsers} />
             </div>
-            <div className="prayer-wall-component-request">
-              <div className="prayer-wall-component-prayer">Prayer Request #2</div>
-            </div>
-            <div className="prayer-wall-component-request">
-              <div className="prayer-wall-component-prayer">Prayer Request #3</div>
-            </div>
-            <div className="prayer-wall-component-request">
-              <div className="prayer-wall-component-prayer">Prayer Request #4</div>
-            </div>
-            <div className="prayer-wall-component-request">
-              <div className="prayer-wall-component-prayer">Prayer Request #5</div>
+            <div className="prayer-header-bottom">
+              <div>&#9734; Answered &#9734;</div>
+              <p>3 Days Ago</p>
             </div>
           </div>
+          <div className="prayer-main">
+            <p>Pray for this app!</p>
+            <FontAwesomeIcon id="prayer-image-icon" icon={faImage} />
+          </div>
+          <div className="prayer-footer">
+            <div className="prayer-footer-left">
+              <FontAwesomeIcon id="prayer-heart-icon" icon={faHeart} />
+              <FontAwesomeIcon id="prayer-thumbs-up-icon" icon={faThumbsUp} />
+              <FontAwesomeIcon id="prayer-comment-icon" icon={faCommentAlt} />
+            </div>
+            <FontAwesomeIcon id="prayer-heart-icon" icon={faEllipsisH} />
+          </div>
         </div>
-    )
+        <div className="prayer-wall-component-request">
+          <div className="prayer-header">
+            <div className="prayer-header-top">
+              <FontAwesomeIcon id="prayer-user-icon" icon={faUser} />
+              <div className="prayer-user">
+                <h2>Christy Liner</h2>
+                <h3>
+                  Prayer <span className="prayer-days-ago">2 Days Ago</span>
+                </h3>
+              </div>
+              <FontAwesomeIcon id="prayer-users-icon" icon={faUsers} />
+            </div>
+            {/* <div className="prayer-header-bottom">
+              <div>&#9734; Answered &#9734;</div>
+              <p>3 Days Ago</p>
+            </div> */}
+          </div>
+          <div className="prayer-main">
+            <p>My husband drives me crazy!</p>
+          </div>
+          <div className="prayer-footer">
+            <div className="prayer-footer-left">
+              <FontAwesomeIcon id="prayer-heart-icon" icon={faHeart} />
+              <FontAwesomeIcon id="prayer-thumbs-up-icon" icon={faThumbsUp} />
+              <FontAwesomeIcon id="prayer-comment-icon" icon={faCommentAlt} />
+            </div>
+            <FontAwesomeIcon id="prayer-heart-icon" icon={faEllipsisH} />
+          </div>
+        </div>
+        <div className="prayer-wall-component-request">
+          <div className="prayer-header">
+            <div className="prayer-header-top">
+              <FontAwesomeIcon id="prayer-user-icon" icon={faUser} />
+              <div className="prayer-user">
+                <h2>JT Liner</h2>
+                <h3>
+                  Prayer <span className="prayer-days-ago">5 Days Ago</span>
+                </h3>
+              </div>
+              <FontAwesomeIcon id="prayer-users-icon" icon={faUsers} />
+            </div>
+            <div className="prayer-header-bottom">
+              <div>&#9734; Answered &#9734;</div>
+              <p>3 Days Ago</p>
+            </div>
+          </div>
+          <div className="prayer-main">
+            <p>Pray for this app!</p>
+            <FontAwesomeIcon id="prayer-image-icon" icon={faImage} />
+            <FontAwesomeIcon id="prayer-images-icon" icon={faImages} />
+
+          </div>
+          <div className="prayer-footer">
+            <div className="prayer-footer-left">
+              <FontAwesomeIcon id="prayer-heart-icon" icon={faHeart} />
+              <FontAwesomeIcon id="prayer-thumbs-up-icon" icon={faThumbsUp} />
+              <FontAwesomeIcon id="prayer-comment-icon" icon={faCommentAlt} />
+            </div>
+            <FontAwesomeIcon id="prayer-heart-icon" icon={faEllipsisH} />
+          </div>
+        </div>
+      </div>
+    );
   }
 }
