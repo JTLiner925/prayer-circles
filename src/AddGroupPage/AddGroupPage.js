bf54ed035dd29b985ef8713a62444e587896f731
JTLiner925 <[jtliner925@gmail.com]>
2020-08-05 14:43:02 -0500

    set up groups call

--- src/AddGroupPage/AddGroupPage.js
+++ src/AddGroupPage/AddGroupPage.js
@@ -1,15 +1,117 @@
-import React, { Component } from 'react'
-import './AddGroupPage.css'
+import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { default as NumberFormat } from "react-number-format";
+import './AddGroupPage.css';
 
 export default class AddGroupPage extends Component {
+  state = {
+    group: '',
+  };
+  submitHandler = (e) => {
+    e.preventDefault();
+    //create group from dashboard
+    this.props.onCreateGroup(this.state);
+  };
+  changeHandler = (e) => {
+    //grabs the group name to join
+    if (e.target.name === 'group_names') {
+      let element = document.querySelector(
+        `#${e.target.value.split(' ').join('_')}`
+      );
+      let groupid;
+
+      groupid = element.getAttribute('groupid');
+
+      this.setState({
+        group_name: e.target.value,
+        groupid: groupid,
+      });
+    } else {
+      this.setState({
+        [e.target.name]: e.target.value,
+      });
+    }
+  };
   render() {
     return (
-      <div className="AddGroupPage">
-        <div className="add-group-form">
-          <div className="add-group-request">
-            <div className='add-group-text'>Add/Edit Group</div>
+      <div className='AddGroupPage'>
+        <form className='add-group-form' onSubmit={this.submitHandler}>
+          <div className='add-group-fontawesome'>
+            <FontAwesomeIcon
+              id='user-icon'
+              className='add-group-avatar'
+              icon={faUser}
+            />
           </div>
-        </div>
+          <label htmlFor='group-name-input' className='add-group-name-label'>
+            <input
+              className='add-group-name'
+              placeholder='Group Name'
+              id='group-name-input'
+              type='text'
+              name='group_name'
+              onChange={this.changeHandler}
+              required
+            ></input>
+          </label>
+          <label htmlFor='pitch-textarea' className='add-group-pitch-textarea-label'>
+            <textarea
+              className='add-group-pitch-textarea'
+              placeholder='Tell us about your group!'
+              id='pitch-textarea'
+              name='pitch'
+              onChange={this.changeHandler}
+              // required
+            ></textarea>
+          </label>
+          <div className='add-group-phone-location-div'>
+            <label htmlFor='phone-input' className='add-group-phone-label'>
+              <NumberFormat
+                id='phone-input'
+                name='leader_phone'
+                className='add-group-phone'
+                placeholder='(###) ###-####'
+                onChange={this.changeHandler}
+                format=' (###) ###-####'
+                mask='_'
+                // required
+              />
+            </label>
+            <label htmlFor='location-input' className='add-group-location-label'>
+              <input
+                className='add-group-location'
+                placeholder='Location'
+                id='location-input'
+                name='group_location'
+                onChange={this.changeHandler}
+              ></input>
+            </label>
+          </div>
+          <label htmlFor='time-date-input' className='add-group-time-date-label'>
+            <input
+              className='add-group-time-date'
+              placeholder='Time and Day i.e. 6pm Fridays'
+              id='time-date-input'
+              name='time_date'
+              onChange={this.changeHandler}
+            ></input>
+          </label>
+          <label htmlFor='more-info' className='add-group-more-info-label'>
+            <textarea
+              className='add-group-more-info'
+              placeholder='More Info'
+              id='more-info'
+              name='more_info'
+              onChange={this.changeHandler}
+              required
+            ></textarea>
+          </label>
+          <p className='error-alert'>{this.props.createMessage}</p>
+          <button type='submit' className='add-group-button'>
+            Create New Group
+          </button>
+        </form>
       </div>
     );
   }
--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import config from '../config'
 import Header from "../Header/Header";
 import ComponentGroupList from '../ComponentGroupList/ComponentGroupList'
 import DashHomePage from "../DashHomePage/DashHomePage";
@@ -14,6 +15,173 @@ import Footer from "../Footer/Footer";
 import "./Dashboard.css";
 
 export default class Dashboard extends Component {
+
+  state={
+    passage: "",
+    users: [],
+    groups: [],
+    events: [],
+    userId: "",
+    groupId: "",
+    eventId: "",
+    error: null,
+    needed: [],
+  }
+  setPassage = (passage) => {
+    this.setState({
+      passage,
+      error: null,
+    });
+  };
+
+  componentDidMount() {
+    let i = window.location.search;
+    let x = new URLSearchParams(i);
+    let eventId;
+
+    for (let [key, value] of x) {
+      if (key === "eventId") {
+        eventId = value;
+      }
+      this.setState({
+        [key]: value,
+      });
+    }
+
+    //call api for data to display in dashboard
+    this.setState({
+      eventMessage: "",
+      
+    });
+    Promise.all([
+      fetch(`${config.HOST}/api/users`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+
+        method: "GET",
+      }),
+      fetch(`${config.HOST}/api/groups`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+        method: "GET",
+      }),
+      fetch(`${config.HOST}/api/events`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+        method: "GET",
+      }),
+      fetch(`${config.HOST}/api/needed`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+        },
+        method: "GET",
+        // body: JSON.stringify({ event_id: eventId }),
+      }),
+    ])
+      .then(([userRes, groupRes, eventRes, needRes]) => {
+        return Promise.all([
+          userRes.json(),
+          groupRes.json(),
+          eventRes.json(),
+          needRes.json(),
+        ]);
+      })
+      .then(([users, groups, events, needed]) => {
+        
+        let userId = users.find(
+          (user) => user.first_name === window.localStorage.getItem("userName")
+        );
+       
+        this.setState({
+          users: users,
+          groups: groups,
+          events: events,
+          userId: userId.id,
+          needed: needed,
+          eventId: eventId,
+        });
+        //maintain bible passage even if page refreshes
+        this.handleBiblePassage(this.state.eventId);
+        
+      })
+      .catch((error) => {
+        this.setState({ error });
+      });
+  }
+handleBiblePassage = (eventId) => {
+    //bible api call
+    let selectedEvent = this.state.events.find((event) => {
+      return event.id.toString() == eventId;
+    });
+    if (selectedEvent) {
+      let url = new URL(`${config.API_ENDPOINT}text/`);
+      url.searchParams.set("q", selectedEvent.bible_passage);
+      url.searchParams.set("include-passage-reference", false);
+      url.searchParams.set("include-verse-number", true);
+      url.searchParams.set("include-first-verse-number", false);
+      url.searchParams.set("include-footnotes", true);
+      url.searchParams.set("include-footnote-body", false);
+      url.searchParams.set("include-heading", false);
+      url.searchParams.set("include-short-copyright", true);
+      url.searchParams.set("indent-using", "tab");
+      const options = {
+        method: "GET",
+
+        headers: {
+          Authorization: `Token ${config.API_KEY}`,
+        },
+      };
+      fetch(url, options)
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error("Something went wrong, please try again later.");
+          }
+          return res.json();
+        })
+        .then((passage) => {
+          this.setPassage(passage);
+          this.setState({
+            eventId: eventId,
+          });
+        })
+        .catch((error) => {
+          this.setState({ error });
+        });
+    }
+  }; 
+
+  createGroup = (formData) => {
+    //add group to api
+    fetch(`${config.HOST}/api/groups/creategroup`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${window.localStorage.getItem("token")}`,
+      },
+      method: "POST",
+      body: JSON.stringify(formData),
+    })
+      .then((res) => {
+        if (!res.ok) {
+          return res.json().then((data) => {
+            return Promise.reject(new Error(data.error.message));
+          });
+        } else {
+          return res.json();
+        }
+      })
+      .then((resData) => {
+        this.props.history.push("/dashboard");
+      })
+      .catch((error) => {
+        this.setState({ createMessage: error.message });
+      });
+  };
   render() {
     return (
       <main className="Dashboard">
@@ -44,7 +212,20 @@ export default class Dashboard extends Component {
         <Route path="/events" component={EventListPage} />
         <Route path="/chat" component={ChatPage} />
         <Route path="/invite" component={InvitePage} />
-        <Route path="/add-group" component={AddGroupPage} />
+        <Route
+          path="/add-group"
+          render={() => {
+            return (
+              <AddGroupPage
+                createMessage={this.state.createMessage}
+                message={this.state.message}
+                groups={this.state.groups}
+                onCreateGroup={this.createGroup}
+                // onJoinGroup={this.joinGroup}
+              ></AddGroupPage>
+            );
+          }}
+        />
         <Route path="/add-event" component={AddEventPage} />
         <Route path="/add-prayer" component={AddPrayerPage} />
         <Route path="/settings" component={SettingsPage} />
